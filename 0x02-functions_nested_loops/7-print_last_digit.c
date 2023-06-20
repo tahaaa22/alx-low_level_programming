ca70122dcff999fdac3708ef7bef19e5ec05129b
root <ahmed.taha.oman@gmail.com>
2023-06-20 16:58:42 -0700

    nested loop tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number.
+ *
+ * @n: takes number input
+ *
+ * Return: lastDigit
+*/
+
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+
+	_putchar(last + '0');
+	return (last);
