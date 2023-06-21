a48299a69363d2476d36089e926ca7ef8279540d
root <ahmed.taha.oman@gmail.com>
2023-06-21 16:20:18 -0700

    nested loop tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,4 +8,23 @@
 
 void print_times_table(int n)
 {
+	int x, min, prod;
+	for (x = 0; x <= n; x++)
+	{
+		_putchar('0');
+		for (min = 1; min <= n; min++)
+		{
+			_putchar(',');
+			_putchar(' ');
 
+			prod = x * min;
+
+			if (n > 15 && n < 0)
+				_putchar(' ');
+			else
+				_putchar(prod + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
