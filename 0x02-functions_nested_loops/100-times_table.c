93ca0baeef722dddbcf800a8db375aaac0b42c6c
root <ahmed.taha.oman@gmail.com>
2023-06-21 15:39:47 -0700

    nested loops tasks

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that printsn times table
+ *
+ * @n: number of times to print
+*/
+
+void print_times_table(int n)
+{
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,7 +15,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar(48);
+		_putchar('0');
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -26,8 +26,8 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 48);
-			_putchar((prod % 10) + 48);
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
