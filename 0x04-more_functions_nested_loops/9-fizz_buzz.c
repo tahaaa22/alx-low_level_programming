88555f1ec108a735273e94b421385299acfc2cb2
root <ahmed.taha.oman@gmail.com>
2023-06-22 16:47:07 -0700

    nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 &&  !(num $ 5 == 0))
+		if (num % 3 == 0 &&  !(num % 5 == 0))
 			printf("Fizz");
 		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
