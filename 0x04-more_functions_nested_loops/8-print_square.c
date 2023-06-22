5df5f8af9da095c40974150e641951af55fd8dd0
root <ahmed.taha.oman@gmail.com>
2023-06-22 16:43:12 -0700

    nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_square - prints a square followed by a new line
+ * print_square - prints a square f
  *
  * @size: is the size of the square
  *
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - print numbers 1 - 100 followed by a new line
+ * 3 fizz
+ * 5 buzz
+ * 3 and 5 fizzbuzz
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 &&  !(num $ 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
