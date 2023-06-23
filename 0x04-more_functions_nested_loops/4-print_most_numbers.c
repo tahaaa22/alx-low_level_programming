a3c1f794ae96278a7080235123e78c0406b8982f
root <ahmed.taha.oman@gmail.com>
2023-06-22 18:50:38 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 apart from 2 and 4
+ *
+ * Return: 0 (Success)
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n = 2 || n = 4)
+			continue;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}
