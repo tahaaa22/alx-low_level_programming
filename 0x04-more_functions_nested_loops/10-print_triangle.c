493daf5275b97f7e570ff1ed413f428c3429de5a
root <ahmed.taha.oman@gmail.com>
2023-06-23 09:07:56 -0700

    nested loops advanced

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,22 +1,30 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle
- * @size: size of the triangle
+ * print_triangle - prints a triangle of '#' characters
+ * @size: the size of the triangle
 */
 
 void print_triangle(int size)
 {
-	int h, b;
+	int i, j;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (h = 1; h <= size; h++)
+		for (i = 1; i <= size; i++)
 		{
-			for (b = 1; b <= h; b++)
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
 				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
