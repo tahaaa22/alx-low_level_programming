bea857b08f19d3380b36713661347044557a9904
root <ahmed.taha.oman@gmail.com>
2023-06-23 08:50:01 -0700

    nested loops advanced

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,14 @@ void print_triangle(int size)
 	int h, b;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-		return;
-	}
-
-	for (h = 1; h <= size; h++)
+	else
 	{
-		for (b = 1; b <= h; b++)
-			_putchar('#');
-		_putchar('\n');
+		for (h = 1; h <= size; h++)
+		{
+			for (b = 1; b <= h; b++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
