40eb4ec9f5402abf5546be783a1bbac8f78254c2
root <ahmed.taha.oman@gmail.com>
2023-06-26 07:42:50 -0700

    Pointers task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: dtring to be reversed
+*/
+
+void rev_string(char *s)
+{
+	int i = 0, n = 0;
+
+	while (s[i])
+		i++;
+	char hold[i + 0];
+
+	hold[i] = '\0';
+
+	while (i--)
+	{
+		hold[n] = s[i];
+		n++;
+	}
+	i = 0;
+
+	while (s[i])
+	{
+		s[i] = hold[i];
+		i++;
+	}
+}
