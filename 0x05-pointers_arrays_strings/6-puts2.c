6f96101d15fe716065df933215f37a2b25216040
root <ahmed.taha.oman@gmail.com>
2023-06-26 07:56:48 -0700

    Pointers task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char of a string starting with first cahr only
+ * @str: string in the parameter
+*/
+
+void puts2(char *str)
+{
+	int i = 0, n;
+
+	while (str[i])
+		i++;
+	for (n = 0; n < i; n += 2)
+		_putchar(str[n];
+	_putchar('\n');
+}
