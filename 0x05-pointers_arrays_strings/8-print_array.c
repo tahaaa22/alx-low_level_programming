b899244cd9526c2f2e7adddd64e250ede19d8224
root <ahmed.taha.oman@gmail.com>
2023-06-26 08:19:11 -0700

    Pointers task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: first pointer
+ * @n: num of elements thats printed
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0, m;
+
+	while (a[i])
+		i++;
+	for (m = 0; m < n; m++)
+	{
+		_putchar(a[m]);
+		if (m != i)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
