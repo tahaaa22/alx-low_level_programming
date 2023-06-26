2b5fa5e9b261bc46f23ee6d4a1cfd4d5c684f7d2
root <ahmed.taha.oman@gmail.com>
2023-06-26 16:57:32 -0700

    Pointers task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,18 +8,25 @@
 
 int _atoi(char *s)
 {
-	int result = 0, digit, sign = 1;
+	int result = 0, digit, sign = 1, i = 0;
 
-	if (s[0] == '-')
+	while (s[i])
 	{
-		sign = -1;
+		if ((s[i] - '0') >= 0 && (s[i] - '0') <= 9)
+		{
+			if (s[i - 1] == '-')
+			{
+				sign = -1;
+			}
+			while ((s[i] - '0') >= 0 && (s[i] - '0') <= 9)
+			{
+				digit = *s - '0';
+				result = result * 10 + digit;
+				s++;
+			}
+			return (sign * result);
+		}
 		s++;
 	}
-	while (*s != '\0')
-	{
-		digit = *s - '0';
-		result = result * 10 + digit;
-		s++;
-	}
-	return (sign * result);
-}
+	return (0);
+};
