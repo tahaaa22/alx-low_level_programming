67b754d2a7f83984dcae1bd568e3d85b5773e524
root <ahmed.taha.oman@gmail.com>
2023-06-27 16:05:34 -0700

    Pointers task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random password for 101crackme file
+ * Return: Always zero
+*/
+
+int main(void)
+{
+	int out;
+
+	srand(time(NULL));
+	out = rand() % 9999;
+	_putchar(out);
+	return (0);
+}
