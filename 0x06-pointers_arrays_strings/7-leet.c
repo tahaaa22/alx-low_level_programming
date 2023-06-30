9342ef5d475889ccffcbc082d56bff7bcf86ca75
root <ahmed.taha.oman@gmail.com>
2023-06-29 19:50:35 -0700

    more pointer tasks

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,27 @@
 #include "main.h"
 
 /**
+ * leet - Encodes a string into 1337
+ * @str: The string to encode
  *
+ * Return: A pointer to the encoded string
+ */
+
+char *leet(char *str)
+{
+	char *leet = "43071";
+	char *letters = "aAeEoOtTlL";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leet[j / 2];
+			}
+		}
+	}
+	return (str);
+}
