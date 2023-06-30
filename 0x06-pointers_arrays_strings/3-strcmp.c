546de1633b11d4ca6fc254b1f65a61f0a13177e8
root <ahmed.taha.oman@gmail.com>
2023-06-29 18:21:32 -0700

    more pointer tasks

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,8 +5,7 @@
  * @s1: The first string
  * @s2: The second string
  *
- * Return: An integer less than, equal to, or greater than zero if s1 is
- *         found, respectively, to be less than, to match, or be greater than s2.
+ * Return: An integer less than, equal to, or greater than zero if s1 is found
  */
 
 int _strcmp(char *s1, char *s2)
