00baabd1cea710f8301b9e9e9235cb517e153537
root <ahmed.taha.oman@gmail.com>
2023-07-01 00:36:00 -0700

    More pointers tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * rot13 - encodes a string using rot 13
+ * @p: pointer to char
+ * Return: pointer to char after encoding
+*/
+
+char *rot13(char *p)
+{
+	int i = 0;
+
+	while (p[i] != '\0')
+	{
+		int j = 0;
+
+		while (p[j + i] >= 65 && p[i + j] <= 90 || p[i + j] >= 97 && p[i + j] <= 122)
+		{
+			if (p[i + j] >= 65 && p[i + j] <= 77 || p[i + j] >= 97 && p[i + j] <= 109)
+				p[i + j] = p[i + j] + 13;
+			else
+				p[i + j] = p[i + j] - 13;
+			j++;
+		}
+		i = i + j + 1;
+	}
+	return (ptr);
