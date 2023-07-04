83adfdba024bab11f645b4a6fce2fc8c72cc6266
root <ahmed.taha.oman@gmail.com>
2023-07-04 04:56:50 -0700

    more pointers task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - function thaat fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: numbers of filled bytes
+ * Return: pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
