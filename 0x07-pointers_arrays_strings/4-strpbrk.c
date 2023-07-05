c78e5b7a3a4650f3cc5d6293edfbce7fcf434b44
root <ahmed.taha.oman@gmail.com>
2023-07-04 18:11:27 -0700

    more pointers tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,10 +2,25 @@
 
 
 /**
- * _strbrk - function that serches a string for any of a set of bytes
+ * _strpbrk - function that serches a string for any of a set of bytes
  * @s: main string
  * @accept: string to check from
  * Return: pointer to the byte in s tha matches in accept
 */
 
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
+}
