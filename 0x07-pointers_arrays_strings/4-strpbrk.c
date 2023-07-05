adc91c655b596972c29e21baaabdca2d549873f8
root <ahmed.taha.oman@gmail.com>
2023-07-04 17:58:28 -0700

    more pointers tasks

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+
+/**
+ * _strbrk - function that serches a string for any of a set of bytes
+ * @s: main string
+ * @accept: string to check from
+ * Return: pointer to the byte in s tha matches in accept
+*/
+
+
