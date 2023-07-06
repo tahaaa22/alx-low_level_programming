8b5fba665fddf266024ec7524030539f7ab87209
root <ahmed.taha.oman@gmail.com>
2023-07-05 17:10:43 -0700

    recursion task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,8 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		s++;
-		_print_rev_recursion(s);
-	}
 	if (*s == '\0')
 		return;
+	_print_rev_recursion(s + 1);
 	printf("%c", *s);
 }
