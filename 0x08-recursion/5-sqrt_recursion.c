ac6f827d67ad27cf7a8802ad9785411587819758
root <ahmed.taha.oman@gmail.com>
2023-07-06 16:58:36 -0700

    recursion tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,21 @@
 #include "main.h"
 
+/**
+ * square - helper function for sqrt
+ * @m: value to get its sqrt
+ * @v: returned result
+ * Return:natral sqrt of m
+*/
+
+int square(int m, int v)
+{
+	if (v * v == m)
+		return (v);
+	else if (v * v < m)
+		return (square(m, v + 1));
+	else
+		return (-1);
+}
 
 /**
  * _sqrt_recursion - function that returns the natural square root of a num
@@ -9,4 +25,5 @@
 
 int _sqrt_recursion(int n)
 {
-
+	return (square(n, 1));
+}
