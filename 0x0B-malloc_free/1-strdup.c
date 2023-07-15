61dee024a7cf9b38f1b5d83df36f46bf0bddfd31
root <ahmed.taha.oman@gmail.com>
2023-07-15 03:14:55 -0700

    malloc tasks

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,13 +17,7 @@ char *_strdup(char *str)
 	for (; str[size] != '\0'; size++)
 		;
 	m = malloc(size * sizeof(*str) + 1);
-
-	if (m == 0)
-		return (NULL);
-	else
-	{
 		for (; i < size; i++)
 			m[i] = str[i];
-	}
 	return (m);
 }
