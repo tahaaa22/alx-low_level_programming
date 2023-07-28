ec0f46def3b83ee9bed5f226123b3cc2265e9cf1
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-07-28 16:00:42 -0700

    single linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_list - print each number of nodes
+ *@h: pointer to the struct list
+ *Return: return number of nodes in the list
+*/
+
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	return(1 + print_list(h->next));
+}
