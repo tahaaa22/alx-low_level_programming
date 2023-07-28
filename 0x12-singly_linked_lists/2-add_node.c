4abfc33ebbd5e6372e077670f47fbd14ba8964ff
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-07-28 16:36:40 -0700

    single linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_node - func that adds a new node at the beginning
+ * @head: pointer to the head
+ * @str: string of the new node
+ * Return: address of the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	struct list_s *a = malloc(sizeof(struct list_s));
+	a->str = strdup(str);
+	while(*str++)
+		i++;
+	a->len = i;
+	a-> next = *head;
+	*head = a;
+}
