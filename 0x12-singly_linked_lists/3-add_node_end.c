0c0b3767fbb4b097481aa90dfe2e49df2cd1d03d
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-07-28 17:57:24 -0700

    single linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -32,8 +32,8 @@ list_t *add_node_end(list_t **head, const char *str)
 		while (temp->next != NULL)
 		{
 			temp = temp->next;
-			temp->next = new_node;
 		}
+		temp->next = new_node;
 	}
 	while (*str++)
 		i++;
