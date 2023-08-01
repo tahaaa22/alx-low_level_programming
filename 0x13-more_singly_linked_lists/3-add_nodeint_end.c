bd39a041513ccf5bb1348cf11477af29df7970fd
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-01 11:37:35 -0700

    more linked list tasks

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - function to add new node at the end
+ * @head: pinter to the struct
+ * @n: int to be added
+ * Return: address of the new element
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newn = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+
+	if (!newn)
+		return (NULL);
+	newn->n = n;
+
+	if (*head == NULL)
+	{
+		*head = newn;
+		newn->next = NULL;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			;
+		temp->next = newn;
+		newn->next = NULL;
+	}
+	return (newn);
+}
