17e9d3eff611bc50d909ed9eb5b9a9ffa1641675
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-01 11:56:59 -0700

    more linked list tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -24,7 +24,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		while (temp->next != NULL)
-			;
+		{
+			temp = temp->next;
+		}
 		temp->next = newn;
 		newn->next = NULL;
 	}
