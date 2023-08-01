e2870b25dfac83feacf93bfe96c267bda82679f0
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-01 13:27:37 -0700

    more linked list tasks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees list
+ * @head: pointer to the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	free_listint2(*head->next);
+	free(head);
+	head = NULL;
+}
