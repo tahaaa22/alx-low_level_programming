cac3459a74e0e28e2a3d77a12312f2c985c3cbf7
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 05:37:24 -0700

    more linked lists tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,24 +10,21 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newn, *temp;
+	listint_t *newn, *temp = *head;
 	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
-	if (idx == 0)
-		return (add_nodeint(&head, n));
-	temp = *head;
-
-	for (i = 0; i < idx - 1 && temp != NULL; i++)
-		temp = temp->next;
-
-	if (i != idx - 1 || temp == NULL)
-		return (NULL);
-	newn = malloc(sizeof(*newn));
+	newn = malloc(sizeof(listint_t));
 
 	if (newn == NULL)
 		return (NULL);
+	if (head == NULL)
+		return (NULL);
+	if (i != idx - 1 || temp == NULL)
+		return (NULL);
+	if (idx == 0)
+		return (add_nodeint(head, n));
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
 	newn->n = n;
 	newn->next = temp->next;
 	temp->next = newn;
