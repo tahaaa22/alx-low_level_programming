3cebb8acd74e298807bca6113775f6f250959c0e
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-02 17:49:41 -0700

    more linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 	if (*head == NULL)
+	{
+		free(newn);
 		return (NULL);
+	}
 	if (newn == NULL)
 		return (NULL);
 	newn->n = n;
