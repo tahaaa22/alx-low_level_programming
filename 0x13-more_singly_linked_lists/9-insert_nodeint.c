ca7fbc9d198faf2997f13195e9b304f2c7644074
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 05:24:48 -0700

    more linked lists tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 	if (idx == 0)
-		return (add_nodeint(head, n));
+		return (add_nodeint(&head, n));
 	temp = *head;
 
 	for (i = 0; i < idx - 1 && temp != NULL; i++)
