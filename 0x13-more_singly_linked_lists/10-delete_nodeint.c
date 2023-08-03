4b1b4bbd816802f9e3ceff4b5b6e604160b2a8e0
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 07:30:57 -0700

    more linked lists tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		{
 			temp = temp2->next;
 			temp2->next = temp->next;
-			t = temp->n;
+			t = 1;
 			free(temp);
 		}
 		temp2 = temp2->next;
