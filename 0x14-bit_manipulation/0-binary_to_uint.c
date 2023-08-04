c02e6d4123f13fbd66d4aae0c982922be56fc13e
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 17:35:25 -0700

    bit manipulation task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function to convert binary to int
+ * @b: pointer to a string
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0, i, n = 0;
+
+	if (!b)
+		return (0);
+	while (*b++ != '\0')
+		n++;
+	for (i = 0; i < n; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			sum = sum + (1 << (n - 1 - i));
+	}
+	return (sum);
+}
