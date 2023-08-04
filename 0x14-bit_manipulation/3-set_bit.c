593a39227a50a5994f7f1836a05bf1d1b397f00d
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 19:45:51 -0700

    bit manipulation task

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - function that sets the value od abit to 1
+ * @n: pointer to the bits
+ * @index: index to be changed
+ * Return: 1 for sucess
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+		return (-1);
+	int i = (n >> index) & 1;
+
+	return (i | (1 << 2));
+}
