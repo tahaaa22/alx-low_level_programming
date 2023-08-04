97794e29e90d12d83e4fd948d56aa633bd9660d7
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 19:59:56 -0700

    bit manipulation task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets the value of a bit to 0
+ * @n: pointer to the long int
+ * @index: index to be converted
+ * Return: 1 for success
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+		return (-1);
+	*n = *n & ~(1 << index);
+
+	return(1);
+}
