be2c9c22c4c4f6c9d14eeef246663523756eaecc
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 19:36:56 -0700

    bit manipulation task

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit
+ * @n: integer to convert
+ * @index: index to be printed
+ * @Return: value at specific index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= 64)
+		return (-1);
+	return ((n >> index) & 1);
+}
