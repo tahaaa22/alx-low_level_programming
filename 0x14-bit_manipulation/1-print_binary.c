e2b244fe121250933bd641818f494a96520237c6
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-03 18:39:06 -0700

    bit manipulation task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,12 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		_putchar('0');
+		_putchar(0 + '0');
 		return;
 	}
 	if (n == 1)
 	{
-		_putchar('1');
+		_putchar(1 + '0');
 		return;
 	}
 	print_binary(n >> 1);
