c43220e070f920c2dbf6631421df9242141e7776
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-08-25 05:06:20 -0700

    late open file task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <elf.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
