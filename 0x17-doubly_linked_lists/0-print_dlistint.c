fd7388158adf23187716f7e3d244d243045a2977
tahaaa22 <ahmed.taha.oman@gmail.com>
2023-09-23 02:54:05 -0700

    doubly linked list tasks in c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints all the elements of double list
+ * @h: pointer to the head
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *temp;
+	int c = 0;
+
+	temp = h;
+
+	if (h == NULL)
+		return (c);
+	while (temp != NULL)
+	{
+		printf("%d\n", h->n);
+		temp = temp->next;
+		c++;
+	}
+	return (c);
+}
